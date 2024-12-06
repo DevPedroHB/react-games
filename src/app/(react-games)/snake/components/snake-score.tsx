48ccc8da85de04b0aa23f8c929e879e5b4786be3
DevPedroHB <pedroh.bergamo20@gmail.com>
2024-12-05 21:41:39 -0300

    📚 docs: Adicionar sumário de teclas

--- src/app/(react-games)/snake/components/snake-score.tsx
+++ src/app/(react-games)/snake/components/snake-score.tsx
@@ -4,9 +4,11 @@ import {
 	Card,
 	CardContent,
 	CardDescription,
+	CardFooter,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { games } from "@/constants/games";
 import { snakeGame } from "@/stores/snake-game";
 import { GAME_STATUS } from "@/types/game-types";
@@ -41,6 +43,44 @@ export function SnakeScore() {
 					<CardTitle>Status:</CardTitle>
 					<CardDescription>{GAME_STATUS[status]}</CardDescription>
 				</div>
+				<Separator className="my-4" />
+				<CardFooter className="flex-col items-start p-0">
+					<CardTitle>Resumo das Teclas:</CardTitle>
+					<div className="mt-4 flex flex-col gap-2">
+						<CardDescription>
+							<strong className="font-semibold text-card-foreground">
+								W, A, S, D
+							</strong>{" "}
+							/{" "}
+							<strong className="font-semibold text-card-foreground">
+								Setas direcionais
+							</strong>{" "}
+							- Movimentam a cobra.
+						</CardDescription>
+						<CardDescription>
+							<strong className="font-semibold text-card-foreground">E</strong>{" "}
+							- Edita as configurações do jogo.
+						</CardDescription>
+						<CardDescription>
+							<strong className="font-semibold text-card-foreground">R</strong>{" "}
+							- Reinicia o jogo.
+						</CardDescription>
+						<CardDescription>
+							<strong className="font-semibold text-card-foreground">P</strong>{" "}
+							- Pausa ou continua o jogo.
+						</CardDescription>
+						<CardDescription>
+							<strong className="font-semibold text-card-foreground">
+								Espaço
+							</strong>{" "}
+							- Inicia o jogo.
+						</CardDescription>
+						<CardDescription>
+							<strong className="font-semibold text-card-foreground">M</strong>{" "}
+							- Volta ao menu.
+						</CardDescription>
+					</div>
+				</CardFooter>
 			</CardContent>
 		</Card>
 	);
